cb66630f418f216c7ee19ab9c63e1cf06a8b05e4
Visgean Skeloru <visgean@gmail.com>
2013-07-28 17:35:26 +0200

    working pausing puasing cancels current interval worker

--- js/controller.js
+++ js/controller.js
@@ -1,10 +1,27 @@
+angular.module('TimerApp', []).
+  filter('digits', function() {
+    return function(input) {
+       if (input < 10) input = '0' + input;
+      
+      return input;
+    }
+  });
+
+
+
+
 function TimeCtrl($scope) {
 	$scope.running= false;
 	$scope.current_index = 0; // at what speech is it now?
+	$scope.time_left = 0; // how much time do we have left?
+	$scope.time_left_m = 0; //minutes
+	$scope.time_left_s = 00; //seconds
 
+	$scope.end_timestamp = false; // current timestamp + time_left
+	$scope.time_holder = document.getElementById("timeholder");
 	$scope.speeches = [
 		{
-			'length': 8*60,
+			'speech_length': 8*60,
 			'speaker': '1st Prop',
 			'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
 			'events' :[
@@ -19,7 +36,7 @@ function TimeCtrl($scope) {
 			]
 		},
 		{
-			'length': 8*60,
+			'speech_length': 8*60,
 			'speaker': '2st Opp',
 			'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
 			'events': [
@@ -36,6 +53,46 @@ function TimeCtrl($scope) {
 	];
 
 
+	$scope.prepare_speaker = function () {
+		// this function sets data for current speaker
+		$scope.running = false;
+		$scope.current_speech = $scope.speeches[$scope.current_index];
+		$scope.time_left = $scope.current_speech.speech_length;
+		$scope.time_left_m = Math.floor($scope.time_left/60);
+		$scope.time_left_s = Math.floor($scope.time_left%60);
 
+	};
+
+
+	$scope.time_updater = function () {
+		if ($scope.running) {
+			var now = new Date().getTime()/1000;
+			diff = $scope.end_timestamp - now;
+			$scope.time_left = diff;
+			$scope.time_left_m = Math.floor(diff/60);
+			$scope.time_left_s = Math.floor(diff%60);
+
+			$scope.$apply();
+		};
+	};
+
+	$scope.Pause = function () {
+		if (!$scope.running) {
+			var now = new Date().getTime()/1000;
+			$scope.end_timestamp = now + $scope.time_left;
+			$scope.running = true;
+			$scope.timer = setInterval($scope.time_updater, 1000);
+		}
+		else {
+			clearInterval($scope.timer);
+			$scope.running = false;
+		};
+	};
+
+	$scope.NextSpeaker = function () {
+	};
+
+
+	$scope.prepare_speaker(); //init values
 
 }
