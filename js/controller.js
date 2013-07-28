9c5165cf624870a87c5e34fb0827b50fa5e42448
Visgean Skeloru <visgean@gmail.com>
2013-07-28 21:07:06 +0200

    all speaches now, showing time for speach

--- js/controller.js
+++ js/controller.js
@@ -1,28 +1,22 @@
 angular.module('TimerApp', []).
-  filter('digits', function() {
-    return function(input) {
-       if (input < 10) input = '0' + input;
-      
-      return input;
-    }
-  });
+filter('digits', function() {
+	return function(input) {
+		if (input < 10 & input >= 0) input = '0' + input;
+
+		return input;
+	}
+});
 
 function TimeCtrl($scope) {
-	$scope.running= false;
 	$scope.current_index = 0; // at what speech is it now?
-	$scope.time_left = 0; // how much time do we have left?
-	$scope.time_left_m = 0; //minutes
-	$scope.time_left_s = 00; //seconds
 
-	$scope.end_timestamp = false; // current timestamp + time_left
-	$scope.time_holder = document.getElementById("timeholder");
 	$scope.speeches = [
-		{
-			'speech_length': 8*60,
-			'speaker': '1st Prop',
+	{
+		'speaker': '1st Prop',
+		'speech_length': 8*60,
 			'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
 			'events' :[
-				{
+			{
 					'name':'Points allowed', // this will be used as subtitle
 					'time_from_start': 60, // when this event happends after start?
 				},
@@ -30,14 +24,14 @@ function TimeCtrl($scope) {
 					'name':'Points forbidden', // this will be used as subtitle
 					'time_from_start': 7*60, // when this event happends after start?
 				}
-			]
-		},
-		{
-			'speech_length': 8*60,
-			'speaker': '2st Opp',
+				]
+			},
+			{
+				'speaker': '1st Opp',
+				'speech_length': 8*60,
 			'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
 			'events': [
-				{
+			{
 					'name':'Points allowed', // this will be used as subtitle
 					'time_from_start': 60, // when this event happends after start?
 				},
@@ -45,27 +39,127 @@ function TimeCtrl($scope) {
 					'name':'Points forbidden', // this will be used as subtitle
 					'time_from_start': 7*60, // when this event happends after start?
 				}
-			]
-		},
-	];
+				]
+			},
+			{
+				'speaker': '2st Prop',
+				'speech_length': 8*60,
+			'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
+			'events': [
+			{
+					'name':'Points allowed', // this will be used as subtitle
+					'time_from_start': 60, // when this event happends after start?
+				},
+				{
+					'name':'Points forbidden', // this will be used as subtitle
+					'time_from_start': 7*60, // when this event happends after start?
+				}
+				]
+			},
+			{
+				'speaker': '2st Opp',
+				'speech_length': 8*60,
+			'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
+			'events': [
+			{
+					'name':'Points allowed', // this will be used as subtitle
+					'time_from_start': 60, // when this event happends after start?
+				},
+				{
+					'name':'Points forbidden', // this will be used as subtitle
+					'time_from_start': 7*60, // when this event happends after start?
+				}
+				]
+			},
+			{
+				'speaker': '3rd Prop',
+				'speech_length': 8*60,
+			'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
+			'events': [
+			{
+					'name':'Points allowed', // this will be used as subtitle
+					'time_from_start': 60, // when this event happends after start?
+				},
+				{
+					'name':'Points forbidden', // this will be used as subtitle
+					'time_from_start': 7*60, // when this event happends after start?
+				}
+				]
+			},
+			{
+				'speaker': '3rd Opp',
+				'speech_length': 8*60,
+			'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
+			'events': [
+			{
+					'name':'Points allowed', // this will be used as subtitle
+					'time_from_start': 60, // when this event happends after start?
+				},
+				{
+					'name':'Points forbidden', // this will be used as subtitle
+					'time_from_start': 7*60, // when this event happends after start?
+				}
+				]
+			},
+			{
+				'speaker': 'Reply Prop',
+				'speech_length': 5*60,
+			'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
+			'events': [
+			{
+					'name':'Points allowed', // this will be used as subtitle
+					'time_from_start': 60, // when this event happends after start?
+				},
+				{
+					'name':'Points forbidden', // this will be used as subtitle
+					'time_from_start': 7*60, // when this event happends after start?
+				}
+				]
+			},
+			{
+				'speaker': 'Reply Opp',
+				'speech_length': 5*60,
+			'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
+			'events': [
+			{
+					'name':'Points allowed', // this will be used as subtitle
+					'time_from_start': 60, // when this event happends after start?
+				},
+				{
+					'name':'Points forbidden', // this will be used as subtitle
+					'time_from_start': 7*60, // when this event happends after start?
+				}
+				]
+			},
+			];
 
 
-	$scope.prepare_speaker = function () {
-		// this function sets data for current speaker
+	$scope.prepare_speaker = function () { // this function sets data for current speaker
 		$scope.running = false;
 		$scope.current_speech = $scope.speeches[$scope.current_index];
 		$scope.time_left = $scope.current_speech.speech_length;
 		$scope.time_left_m = Math.floor($scope.time_left/60);
 		$scope.time_left_s = Math.floor($scope.time_left%60);
 
+		$scope.progress = 00; //seconds
 	};
 
 
 	$scope.time_updater = function () {
 		if ($scope.running) {
 			var now = new Date().getTime()/1000;
-			diff = $scope.end_timestamp - now;
-			$scope.time_left = diff;
+			
+
+			if ($scope.end_timestamp<now) { // uz jsme v minusu
+				$scope.progress = 100;
+				diff = now - $scope.end_timestamp;
+			}
+			else {
+				diff = $scope.end_timestamp - now;
+				$scope.time_left = diff;
+				$scope.progress = 100 - Math.floor(($scope.time_left/ $scope.current_speech.speech_length)*100);
+			};
+			
 			$scope.time_left_m = Math.floor(diff/60);
 			$scope.time_left_s = Math.floor(diff%60);
 
@@ -93,6 +187,12 @@ function TimeCtrl($scope) {
 		else {
 			$scope.current_index = 0;
 		};
+
+		$scope.current_speech.real_length = Math.floor($scope.current_speech.speech_length - $scope.time_left);
+		
+		$scope.current_speech.real_length_m = Math.floor($scope.current_speech.real_length/60);
+		$scope.current_speech.real_length_s = Math.floor($scope.current_speech.real_length%60);
+
 		$scope.prepare_speaker();
 	};
 
