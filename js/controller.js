cdf5fbab306a94863100f272ea37f283746fd0bc
Visgean Skeloru <visgean@gmail.com>
2013-07-28 09:29:56 +0200

    working angular

---
+++ js/controller.js
@@ -0,0 +1,40 @@
+function TimeCtrl($scope) {
+
+  $scope.speeches = [
+    {
+    	'length': 8*60,
+    	'speaker': '1st Prop',
+    	'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
+    	'events' :[
+    		{
+    			'name':'Points allowed', // this will be used as subtitle
+    			'time_from_start': 60, // when this event happends after start?
+    		},
+    		{
+    			'name':'Points forbidden', // this will be used as subtitle
+    			'time_from_start': 7*60, // when this event happends after start?
+    		}
+    	]
+    },    
+    {
+    	'length': 8*60,
+    	'speaker': '2st Opp',
+    	'offset' : 30, //how much time does the speaker have after the 'end' of the debate before MUTE
+    	'events': [
+    		{
+    			'name':'Points allowed', // this will be used as subtitle
+    			'time_from_start': 60, // when this event happends after start?
+    		},
+    		{
+    			'name':'Points forbidden', // this will be used as subtitle
+    			'time_from_start': 7*60, // when this event happends after start?
+    		}
+    	]
+    },
+
+  ];
+
+
+
+
+}
